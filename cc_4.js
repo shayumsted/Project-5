87d6c24a5dcb4e3efc1d8936260c65616702152e
Shamira Umstead <sumstead@usf.edu>
2024-05-22 13:52:28 -0400

    git commit -m "Implement Employee class with name, salary properties, and annual salary calculation."

--- cc_4.js
+++ cc_4.js
@@ -1,2 +1,9 @@
 // U78784426
 
+// Defining the employee class
+class Employee {constructor(name, salary) {this.name = name; this.salary = salary;
+        console.log(`Employee: ${this.name}, Monthly Salary: $${this.salary}`);}
+
+// Calculating the annual salary
+calculateAnnualSalary() {return this.salary * 12;}}
+
